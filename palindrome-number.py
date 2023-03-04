ed8dc167ad4d6a3aa9a7226b36004c02c827cbfa
pekka1234 <62663286+pekka1234@users.noreply.github.com>
2023-03-04 16:59:03 +0200

    Update palindrome-number.py

--- palindrome-number.py
+++ palindrome-number.py
@@ -1,6 +1,9 @@
 class Solution(object):
     def isPalindrome(self, x):
         if x > 9:
+            # number is turned to digit list
+            # middle of the list is taken, rounded downwards becouse it doesn't matter if the exact middle stays unchekced
+            # if the start of the list reversed is the end of the list reversed, then it is palindrome
             l = [int(x) for x in str(x)]
             halflen = int((len(l) / 2))
             if list(reversed(l[:halflen])) == l[-halflen:]:
