c438a24e3b3ff62c9d1f0c112d0e13968354226a
pekka1234 <62663286+pekka1234@users.noreply.github.com>
2023-03-08 16:24:24 +0200

    Update roman-to-integer.py

--- roman-to-integer.py
+++ roman-to-integer.py
@@ -1,10 +1,10 @@
 class Solution(object):
     def romanToInt(self, s):
-        rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        int_val = 0
+        romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        anwser = 0
         for i in range(len(s)):
-            if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
-                int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
+            if i > 0 and romans[s[i]] > romans[s[i - 1]]:
+                anwser += romans[s[i]] - 2 * romans[s[i - 1]]
             else:
-                int_val += rom_val[s[i]]
-        return int_val
+                anwser += romans[s[i]]
+        return anwser
